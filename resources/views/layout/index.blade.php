c1ba06fb356d76f7cb9d04eb5e8f70c3f16341d7
digitaldev-lx <pauloamserrano@gmail.com>
2022-06-24 18:21:33 +0100

    fix metas to psiquiatria

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -12,8 +12,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>XVI Congresso Nacional de Psiquiatria</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Aterosclerose - GETDONE"/>
-    <meta name="keywords" content="Aterosclerose - GETDONE"/>
+    <meta name="description" content="XVI Congresso Nacional de Psiquiatria"/>
+    <meta name="keywords" content="XVI Congresso Nacional de Psiquiatria"/>
     <meta name="author" content="codeboys.pt"/>
 
 
