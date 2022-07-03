3f02f9d0990ec7543e4f98642d7c30b36ed56959
Paulo Serrano <pauloamserrano@gmail.com>
2022-07-03 17:02:52 +0100

    convidados. páginas individuais e css

---
+++ storage/framework/views/1be303307abf756167e6aafe7237ace36b9eb6fa.php
@@ -0,0 +1,38 @@
+<?php $__env->startSection("content"); ?>
+    <div id="section-convidados">
+        <img src="<?php echo e(asset("images/img-top-convidados.png")); ?>" class="img-top-convidados" alt="Sociedade Portuguesa de Psiquiatria"/>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-md-2 col-md-offset-1">
+                    <img alt="Amelia Fiske" class="doctor-img-individual" src="<?php echo e(asset("images/convidados/LAUREL_RIEK.jpg")); ?>"/>
+                    <img src="<?php echo e(asset("images/sppsm_logo.png")); ?>" style="width: 150px; float: left; margin-top:35vh" alt="sppsm"/>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <h2 class="fh5co-section-convidados">Laurel Riek</h2>
+                    <h4>USA</h4>
+                    <p>Dr. Riek is a professor of Computer Science and Engineering at UC San Diego, and holds
+                        affiliate appointments in the Department of Emergency Medicine, Contextual Robotics Institute,
+                        and Design Lab. Dr. Riek is a pioneer in the field of healthcare robotics, and has demonstrated
+                        an ongoing record of research accomplishment in the fields of human robot
+                        interaction, health informatics, and artificial intelligence. Dr. Riek directs the Healthcare
+                        Robotics Lab, which engages in community-centered health technology design research to
+                        develop new assistive technologies that can support people with cognitive and physical
+                        disabilities, their families, and their communities. The lab also engages in signifiant research
+                        efforts within in acute care environments, to create technologies that can help alleviate
+                        healthcare worker burnout and improve patient experience.<br><br>
+                        Dr. Riek received a Ph.D. in Computer Science from the University of Cambridge, and B.S. in
+                        Logic and Computation from Carnegie Mellon. Riek served as a Senior Artificial Intelligence
+                        Engineer and Roboticist at The MITRE Corporation from 2000-2008, and held the Clare Boothe
+                        Luce chair in Computer Science and Engineering at the University of Notre Dame from
+                        2011-2016. Dr. Riek has received the NSF CAREER Award, AFOSR Young Investigator Award,
+                        Qualcomm Research Award, and several best paper awards. Dr. Riek is the HRI 2023 General
+                        Co-Chair and served as the Program Co-Chair for HRI 2020, and also serves on the editorial
+                        boards of T-RO and THRI.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make("layout.index", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/resources/views/convidados/tom-craig.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4ceb262ff886a44723fd195181e341fb75786d3f.php
@@ -0,0 +1,37 @@
+<?php $__env->startSection("content"); ?>
+    <div id="section-convidados">
+        <img src="<?php echo e(asset("images/img-top-convidados.png")); ?>" class="img-top-convidados" alt="Sociedade Portuguesa de Psiquiatria"/>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-md-2 col-md-offset-1">
+                    <img alt="Amelia Fiske" class="doctor-img-individual" src="<?php echo e(asset("images/convidados/IRMGARD_EMMELHAINZ.jpg")); ?>"/>
+                    <img src="<?php echo e(asset("images/sppsm_logo.png")); ?>" style="width: 150px; float: left; margin-top:35vh" alt="sppsm"/>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <h2 class="fh5co-section-convidados">Irmgard Emmelhainz</h2>
+                    <h4>MEXICO</h4>
+                    <p>Irmgard Emmelhainz is a professor, writer, researcher and translator based in Anahuac Valley
+                        (Mexico City). She holds a Ph.D. from the Art Department at the University of Toronto and an
+                        M.A. in Art History, Theory and Criticism from The School of the Art Institute of Chicago. Her
+                        work about film, the Palestine Question, art, culture and neoliberalism has been translated to
+                        many languages and she has presented it at an array of international venues, including Harvard,
+                        the March Meeting at Sharjah, the Walter Benjamin in Palestine Conference (2015) the New
+                        School and Americas Society (2016), SBC Gallery, Montreal (2016), The University of California
+                        in San Diego, ArtBo, Bogotá, School of Visual Arts, New York, KHIO at Oslo, University of Texas
+                        El Paso (2020), Stanford University (2021), MoMA (2022), etc. Her book The Tyranny of
+                        Common Sense: Mexico’s Postneoliberal Conversion was published in English with SUNY Press
+                        in 2020; The Sky is Incomplete: Travel Chronicles in Palestine was published by Taurus Mexico
+                        (2017) and is forthcoming with Vanderbilt University Press (2023). There is alsoJean-Luc
+                        Godard’s Political Filmmaking by Palgrave Macmillan in 2019. This year, Toxic Loves, Impossible
+                        Futures: Feminist Lives as Resistance came out in the Critical Mexican Studies with Vanderbilt
+                        (2021) and will be published in Spanish by Taurus. Finally, Despachos de otro mundo
+                        (Ejercicios de imaginación radical) is going to be published with Bajo Tierra Ediciones (México,
+                        2022).</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make("layout.index", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/resources/views/convidados/irmgard-emmelhainz.blade.php ENDPATH**/ ?>
